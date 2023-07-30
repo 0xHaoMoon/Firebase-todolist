bdf8bef2576323cb2f02dbf4897d063274d1d78c
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-07-30 16:14:55 +0200

    show todo in htnl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,28 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
-  item$: Observable<any>;
-  firestore: Firestore = inject(Firestore);
+  todos$: Observable<any>; // Obersavle ist eine Variable die sich updatet, <any> könnte auch ein string oder number sein. Deshalb any 
+  // alternative
+  todos!: Array<any>; 
+
+  todotext = '';
+
+  firestore: Firestore = inject(Firestore); // Importieren des Objektes 'Firestore' aus import
 
   constructor() {
-    const itemCollection = collection(this.firestore, 'items');
-    this.item$ = collectionData(itemCollection);
+    const todosCollection = collection(this.firestore, 'todos'); // greift auf die collection im firestore zu mit dem namen 'todos' 
+    this.todos$ = collectionData(todosCollection); // aus der collection greifen wir nun auf alle daten zu und geben es der globalen varible
+    
+    //alternative
+    this.todos$.subscribe((newTodos)=>{ // abrufen der neusten Daten, wenn sich daten im Datenbank ändert, wird diese aufegerufen                      
+      console.log(newTodos)
+      this.todos = newTodos; // damit global auf die variable todos zugegriffen werden kann
+    })
+  }
+
+  //hinzufügen daten in datenbank
+  addTodo(){
+
   }
 
 }
